33330a3b78b2450143de7734b77cce4b5e278d95
Joel-Crunk <joel.crunk@outlook.com>
2019-06-20 16:31:03 -0400

    Add project files.

---
+++ HumorHacker/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HumorHacker.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Joel Crunk";
+
+            return View();
+        }
+        public ActionResult Memes()
+        {
+            ViewBag.Message = "Your memes page.";
+
+            return View();
+        }
+    }
+}
