e5e33a026aef602848fdfebba38f45e685e61f70
Joel-Crunk <joel.crunk@outlook.com>
2019-06-26 08:10:46 -0400

    This commit is a functioning website

--- HumorHacker/DAL/ProductInitializer.cs
+++ HumorHacker/DAL/ProductInitializer.cs
@@ -9,17 +9,18 @@ using System.Data.Entity;
 namespace HumorHacker.DAL
 {
 
-    public class ProductInitializer : DropCreateDatabaseIfModelChanges<ProductContext>
+    public class ProductInitializer : DropCreateDatabaseAlways<ProductContext>
     {
         protected override void Seed(ProductContext context)
         {
             var products = new List<Product>
             {
-            new Product{Name="TShirt",Category="Clothes", Price=750},
-            new Product{Name="Coffee mug",Category="Swag", Price=750},
-            new Product{Name="mouse pad",Category="Swag", Price=750},
-            new Product{Name="Computer Desk",Category="Office", Price=750},
-            new Product{Image=$"/Images/debugging on toilet meme.JPG", Name="Phone Charger",Category="Misc.", Price=750},
+            new Product{Image="/Images/TShirt.jpg",Name="TShirt",Category="Clothes", Price=7},
+            new Product{Image="/Images/CoffeeMug.jpg",Name="Coffee mug",Category="Swag", Price=15},
+            new Product{Image="/Images/MousePad.jpg",Name="mouse pad",Category="Swag", Price=5},
+            new Product{Image="/Images/ComputerDesk.jpg",Name="Computer Desk",Category="Office", Price=750},
+            new Product{Image="/Images/PhoneCharger.JPG", Name="Phone Charger",Category="Misc.", Price=5},
+            //new Product{Image="/Images/My feature their bug.JPG", Name="Phone Charger",Category="Misc.", Price=750}
 
             };
 
